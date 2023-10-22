f29f6308ca65f123b9689188079de27454d7e172
luc <lucmoulinveillon@gmail.com>
2023-10-22 18:18:51 -0400

    ADD : letterbox to csv

--- src/webscrapping_letterboxd/main.py
+++ src/webscrapping_letterboxd/main.py
@@ -1,23 +1,70 @@
+import csv
+import numpy as np
 import requests
 from bs4 import BeautifulSoup
 
-def goThroughYear(year):
-    urlList = "https://letterboxd.com/films/ajax/popular/year/" + str(year) + "/?esiAllowFilters=true"
-    soup = BeautifulSoup(requests.get(urlList).text, 'html.parser')
-    movies = soup.find_all('li', {'class': "listitem poster-container"})
-    for movie in movies:
-        div_movie = movie.find("div")
-        link = "https://letterboxd.com" + div_movie.get('data-target-link') + "genres/"
-        getMovieGenre(link)
+
+def goThroughYear(years):
+    return_result = []
+    for year in years:
+        result = {"year": year, "movies": []}
+        urlList = "https://letterboxd.com/films/ajax/popular/year/" + \
+            str(year) + "/?esiAllowFilters=true"
+        soup = BeautifulSoup(requests.get(urlList).text, 'html.parser')
+        movies = soup.find_all('li', {'class': "listitem poster-container"})
+        for movie in movies:
+            div_movie = movie.find("div")
+            link = "https://letterboxd.com" + \
+                div_movie.get('data-target-link') + "genres/"
+            result["movies"].append(getMovieGenre(link))
+        return_result.append(result)
+    return return_result
+
 
 def getMovieGenre(link):
-    soup = BeautifulSoup(requests.get(link).text, 'html.parser')
-    genres_div = soup.find('div', {'id': "tab-genres"})
-    sub_genres_div = genres_div.find_all('div')
-    if (len(sub_genres_div) == 2):
-        genres = sub_genres_div[1].find_all('a')
-        for genre in genres:
-            print(genre.text.strip())
-    
+    result = {"name": "?", "genres": [], "themes": []}
 
-goThroughYear(2021)
+    soup = BeautifulSoup(requests.get(link).text, 'html.parser')
+    print(soup.find("h1", {'class': "prettify"}).get_text())
+    result["name"] = soup.find("h1", {'class': "prettify"}).get_text()
+    genres_and_theme_div = soup.find('div', {'id': "tab-genres"})
+    sub_genres_and_theme_div = genres_and_theme_div.find_all(
+        'div', {"class": "text-sluglist"})
+    if (len(sub_genres_and_theme_div) == 2):
+        genres = sub_genres_and_theme_div[0].find_all('a')
+        themes = sub_genres_and_theme_div[1].find_all('a')
+        # print(" == Genres ==")
+        # print(genres)
+        for genre in genres:
+            # print(genre.text.strip())
+            result["genres"].append(genre.text.strip())
+
+        # print(" == Themes ==")
+        for theme in themes:
+            if "Show All…" not in theme.text.strip():
+                result["themes"].append(theme.text.strip())
+    return result
+
+
+def save_to_csv(filename, data):
+    with open(filename, mode='w', newline='') as file:
+        fieldnames = ["year", "name", "genres", "themes"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for year in data:
+            for item in year["movies"]:
+                # Convert the "genres" and "themes" lists to comma-separated strings
+                item["year"] = year["year"]
+                item["genres"] = ', '.join(item["genres"])
+                item["themes"] = ', '.join(item["themes"])
+                print(item)
+                writer.writerow(item)
+
+
+list_date = np.arange(2010, 2023, 1)
+print(list_date)
+
+data_to_save = goThroughYear(list_date)
+
+
+save_to_csv('output.csv', data_to_save)
