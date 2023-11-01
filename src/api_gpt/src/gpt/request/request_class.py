905726e6944fe8885e58788ae4f9b03825524298
luc <lucmoulinveillon@gmail.com>
2023-11-01 17:35:41 -0400

    ADD : title and description with GPT4

--- src/api_gpt/src/gpt/request/request_class.py
+++ src/api_gpt/src/gpt/request/request_class.py
@@ -1,40 +1,54 @@
 
+import random
 import re
 from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
+import requests
 
 
-async def find_movie_title(type_movie: str) -> str:
+async def find_movie_title(type_movie: str, type_role : str = "oscar", word : str = "invention") -> str:
     result: str = ""
-    prompt_movie = f"Génère une description de film imaginaire dans le genre {type_movie} , sans référence à des films existants et sans considération pour l'actualité. Il faut que le film soit plutot orienté pour avoir des oscars donc un film d'horreur mais aussi un peu d'auteur"
+    prompt_movie = f"Crée une description de film d'{type_role} dans le genre {type_movie}, basée sur le mot du jour :{word}. Donne moi dans un premier temps le titre et ensuite la description du film et seulement ca. exemple Titre : ... \n Description : ..."
     conversation_style_movie = ConversationStyle.creative
 
     bot = await Chatbot.create()  # Passing cookies is "optional", as explained above
     response = await bot.ask(prompt=prompt_movie, conversation_style=conversation_style_movie, simplify_response=True)
     if "\n\n" in response.get("text"):
-        print(response)
         text = response.get("text")
-        # Define regular expression patterns
-        pattern_title = r'\*\*(.*?)\*\*'
-        pattern_description = r'\*\*\n\n(.*?)$'
+        # print(text)
+        # print("====================")
+        # Utilisez des expressions régulières pour extraire le titre et la description
+        
+        titre_match = re.search(r'Titre : (.+)', text)
+        description_match = re.search(r'Description : (.+)', text)
 
-        # Use re.search() to find the title
-        title_match = re.search(pattern_title, text)
-        if title_match:
-            title = title_match.group(1)
+        if titre_match:
+            titre = titre_match.group(1).replace("*","")
         else:
-            title = "No title found"
+            titre = "Titre non trouvé"
 
-        # Use re.search() to find the description
-        description_match = re.search(pattern_description, text)
         if description_match:
-            description = description_match.group(1)
+            description = description_match.group(1).replace("*","")
         else:
-            description = "No description found"
-        print("Title:", title)
-        print("Description:", description)
+            description = "Description non trouvée"
 
+
+
+
+    return titre ,description
+
+
+async def hazard_word():
+
+
+    # Faites une requête à l'API pour obtenir un mot aléatoire
+    response = requests.get("https://random-word-api.herokuapp.com/word")
+
+    # Vérifiez si la requête a réussi
+    if response.status_code == 200:
+        # Le mot aléatoire est dans la réponse JSON
+        mot_aleatoire = response.json()[0]
+        # print("Mot aléatoire :", mot_aleatoire)
     else:
-        print("================ ERROR ================")
-        print(response.get("text"))
-
-    return result
+        print("Échec de la requête à l'API")
+    
+    return mot_aleatoire
