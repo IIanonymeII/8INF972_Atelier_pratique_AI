9ef1d7d85389da39703edcaabd29829b07ab49cc
Courbayre Basil <basil.courbayre@ecole.ensicaen.fr>
2023-11-03 02:13:57 -0400

    updated missing values

--- src/the_numbers/joinData.py
+++ src/the_numbers/joinData.py
@@ -5,6 +5,7 @@ from sklearn.impute import KNNImputer, SimpleImputer
 dfs = [pd.read_csv("movies" + str(year) + ".csv") for year in range(1930, 2022)]
 df = pd.concat(dfs, ignore_index=True)
 
+
 # Step 2: Handle missing values
 # For 'MPAA Rating', replace missing values with the most common one amongst the 1 nearest neighbors
 df['MPAA Rating'] = df["MPAA Rating"].fillna("Not Rated")
@@ -18,9 +19,15 @@ df[columns_to_fill_with_zero] = df[columns_to_fill_with_zero].fillna(0)
 df['Total original B.O'] = df['Original domestic B.O'] + df['Original international B.O']
 df['Total adjusted B.O'] = df['Adjusted domestic B.O'] + df['Adjusted international B.O']
 
+"""
+missing_values_per_column = df.isna().sum()
+print(missing_values_per_column)
+"""
+
 columns_to_impute = ["Year", "Month"]
 knn_imputer = KNNImputer(n_neighbors=5, weights='distance')
 df[columns_to_impute] = knn_imputer.fit_transform(df[columns_to_impute])
+
 
 columns_to_impute = ["Original budget", "Adjusted budget"]
 median_imputer = SimpleImputer(strategy='median')
