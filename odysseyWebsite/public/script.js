964981c29885344d1a86839cd1bf28de496ee103
Courbayre Basil <basil.courbayre@ecole.ensicaen.fr>
2023-12-09 18:06:19 -0500

    update page principale

--- odysseyWebsite/public/script.js
+++ odysseyWebsite/public/script.js
@@ -6,3 +6,34 @@ loginButton.addEventListener('click', function() {
   // Navigate to the search.html page
   window.location.href = 'search.html';
 });
+
+let currentSection = 1;
+
+window.addEventListener('wheel', (event) => {
+    if (event.deltaY > 0) {
+        // Scrolling down
+        currentSection++;
+    } else {
+        // Scrolling up
+        currentSection--;
+    }
+
+    if (currentSection < 1) {
+        currentSection = 1;
+    } else if (currentSection > 6) {
+        currentSection = 6;
+    }
+
+    scrollToSection(currentSection);
+});
+
+function scrollToSection(section) {
+    const sectionId = `section${section}`;
+    const sectionElement = document.getElementById(sectionId);
+    window.scrollTo({
+        top: sectionElement.offsetTop,
+        behavior: 'smooth'
+    });
+}
+
+scrollToSection(currentSection);
